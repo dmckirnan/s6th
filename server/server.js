46081c0e0940014567395532a23496bd85c78d2c
Darrick Mckirnan <dmckirnan@hotmail.com>
2019-07-27 22:42:36 -0700

    Dynamically assign PORT in Server

--- server/server.js
+++ server/server.js
@@ -4,4 +4,4 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
-const server = app.listen(8000, () => console.log('Listening on 8000'));
+const server = app.listen(process.env.PORT || 8000, () => console.log('Listening on 8000'));
